bca9404ed36763372835ff95061a43404cbcab76
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-03-24 15:37:18 +0000

    feat: add Fetch model
    Sets up a basic TypeScript project which contains Fetch, Request and Response
    types and tests they are compatible with both node-fetch and WHATWG Fetch.

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+/**
+ * @since 0.1.0
+ */
+
+// -------------------------------------------------------------------------------------
+// model
+// -------------------------------------------------------------------------------------
+
+/**
+ * @category model
+ * @since 0.1.0
+ */
+export type Fetch = (...args: Request) => Promise<Response>
+
+/**
+ * @category model
+ * @since 0.1.0
+ */
+export type Request = [url: string, init: RequestInit]
+
+type RequestInit = {
+  readonly body?: string
+  readonly headers: Record<string, string>
+  readonly method: string
+}
+
+/**
+ * @category model
+ * @since 0.1.0
+ */
+export interface Response {
+  readonly headers: Headers
+  readonly status: number
+  readonly statusText: string
+  readonly url: string
+
+  text(): Promise<string>
+}
+
+interface Headers extends Iterable<[string, string]> {
+  get(name: string): string | null
+
+  has(name: string): boolean
+}
---
+++ test-d/ts4.0/index.ts
@@ -0,0 +1,9 @@
+import nodeFetch from 'node-fetch'
+import * as _ from '../../src'
+
+//
+// Fetch
+//
+
+const f1: _.Fetch = fetch
+const f2: _.Fetch = nodeFetch
